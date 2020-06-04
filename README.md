ac3413dd525a7daa672a7920eff47a3f1ec22d16
G-T <guillaume.tripier1@gmail.com>
2020-06-04 16:46:33 +0200

    calcul stats points moyen de france et adversaire

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # spark_project
 
-##Les données sont filtrés pour supprimer les lignes qui ne sont pas correctement bien formatés
+## Les données sont filtrés pour supprimer les lignes qui ne sont pas correctement bien formatés
--- esgi/exo/FootballApp/FootballApp.py
+++ esgi/exo/FootballApp/FootballApp.py
@@ -3,20 +3,31 @@ from datetime import datetime
 from pyspark.sql import SparkSession, Window, SQLContext
 from pyspark import SparkConf
 import pyspark.sql.functions as F
-from pyspark.sql.types import StringType, DateType, StructType, StructField, IntegerType
+from pyspark.sql.types import StringType, DateType, StructType, StructField, IntegerType, BooleanType
 
 class FootballApp():
 
 	@staticmethod
 	def change_penality_null_by_0(penaltyValue):
-		penaltyValue = str(penaltyValue)
 		if penaltyValue == "NA" :
 			return 0 
 		return int(penaltyValue)
 
+	@staticmethod
+	def filter_after_date(targetDate, dfToFilter):
+		return dfToFilter.filter(dfToFilter.date >= targetDate)
+
+	@staticmethod
+	def is_match_played_at_home(match, adversaire):
+		mathcSplited = match.split(" - ")
+		if len(mathcSplited) == 2 and mathcSplited[1] == adversaire :
+			return True 
+		return False
+
 	def __init__(self, argv):
 		self.spark = SparkSession.builder.appName("my-spark-app").config("spark.ui.port","5050").getOrCreate()
 		self.change_penality_null_by_0_udf = F.udf(self.change_penality_null_by_0, IntegerType())
+		self.is_match_played_at_home_udf = F.udf(self.is_match_played_at_home, BooleanType())
 	
 	def load_dataFrame_from_csv(self, csvFilePath):
 		schema = StructType([
@@ -32,27 +43,32 @@ class FootballApp():
 			StructField("date", DateType(), True),
 			StructField("year", IntegerType(), True),
 			StructField("outcome", StringType(), True),
-			StructField("no", StringType(), True),
+			StructField("no", StringType(), True)
 		])
 		dfNotFiltered = SQLContext(self.spark).read.csv(csvFilePath, header=True,schema=schema)
 		return dfNotFiltered.filter(dfNotFiltered.no != "None")
 
-	@staticmethod
-	def filter_after_date(targetDate, dfToFilter):
-		return dfToFilter.filter(dfToFilter.date >= targetDate)
-
+	def step_one_clean_data(self, dfInput):
+		dfInput = dfInput.withColumnRenamed("X4", "match").withColumnRenamed("X6", "competition")
+		dfInput = dfInput.select("match", "competition", "adversaire", "score_france", "score_adversaire", "penalty_france", "penalty_adversaire", "date")
+		dfInput = dfInput.withColumn('penalty_france', self.change_penality_null_by_0_udf(dfInput.penalty_france))
+		dfInput = dfInput.withColumn('penalty_adversaire', self.change_penality_null_by_0_udf(dfInput.penalty_adversaire))
+		return self.filter_after_date(datetime(1980, 3, 1), dfInput)
 
 	def main(self):
-		df_matches = self.load_dataFrame_from_csv("res/df_matches.csv")
-		df_matches = df_matches.withColumnRenamed("X4", "match")
-		df_matches = df_matches.withColumnRenamed("X6", "competition")
-		df_matches = df_matches.select("match", "competition", "adversaire", "score_france", "score_adversaire", "penalty_france", "penalty_adversaire", "date")
-		df_matches = df_matches.withColumn('penalty_france', self.change_penality_null_by_0_udf(df_matches.penalty_france))
-		df_matches = df_matches.withColumn('penalty_adversaire', self.change_penality_null_by_0_udf(df_matches.penalty_adversaire))
-		df_matches = self.filter_after_date(datetime(1980, 3, 1), df_matches)
+		dfMatches = self.load_dataFrame_from_csv("res/df_matches.csv")
+		dfMatchesFiltered = self.step_one_clean_data(dfMatches)
+		#dfMatchesFiltered.persist()
+		dfMatchesFiltered = dfMatchesFiltered.withColumn('is_at_home', self.is_match_played_at_home_udf(dfMatchesFiltered.match, dfMatchesFiltered.adversaire))
+		
+		windowByAdversaire = Window.partitionBy("adversaire")
 
-		df_matches.printSchema()
-		df_matches.show(100)#5)
+		avgScoreFrance = F.avg("score_france").over(windowByAdversaire)
+		avgScoreAdversaire = F.avg("score_adversaire").over(windowByAdversaire)
+		dfStats = dfMatchesFiltered.withColumn("avg_score_france", avgScoreFrance).withColumn("avg_score_adversaire", avgScoreAdversaire)
+		
+		dfStats.printSchema()
+		dfStats.show(20)
 
 if __name__ == '__main__':
 	footballApp = FootballApp(sys.argv)
